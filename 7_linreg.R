358d7b71f4c4f0d7821b4ba735d619dabc324737
Gerald Hampapa <gerald.hampapa@gmx.net>
2018-10-31 07:50:26 +0100

    Update

--- 7_linreg.R
+++ 7_linreg.R
@@ -412,4 +412,147 @@ fit <- Teams %>%
            BB=BB/G,
            R=R/G) %>%
     lm(R ~ BB + singles + doubles + triples + HR, data=.)
-tidy(fit, conf.int=TRUE)
+coefs <- tidy(fit, conf.int=TRUE)
+
+Teams %>%
+    filter(yearID %in% 2002) %>%
+    mutate(HR=HR/G,
+           singles=(H-X2B-X3B-HR)/G,
+           doubles=X2B/G,
+           triples=X3B/G,
+           BB=BB/G,
+           R=R/G) %>%
+    mutate(R_hat=predict(fit, newdata=.)) %>%
+    ggplot(aes(R_hat, R, label = teamID)) + 
+    geom_point() +
+    geom_text(nudge_x=0.05, cex = 2) + 
+    geom_abline()
+
+pa_per_game <- Batting %>% filter(yearID == 2002) %>% 
+    group_by(teamID) %>%
+    summarize(pa_per_game = sum(AB+BB)/max(G)) %>% 
+    .$pa_per_game %>% 
+    mean
+
+players <- Batting %>% filter(yearID %in% 1999:2001) %>% 
+    group_by(playerID) %>%
+    mutate(PA = BB + AB) %>%
+    summarize(G = sum(PA)/pa_per_game,
+              BB = sum(BB)/G,
+              singles = sum(H-X2B-X3B-HR)/G,
+              doubles = sum(X2B)/G, 
+              triples = sum(X3B)/G, 
+              HR = sum(HR)/G,
+              AVG = sum(H)/sum(AB),
+              PA = sum(PA)) %>%
+    filter(PA >= 300) %>%
+    select(-G) %>%
+    mutate(R_hat = predict(fit, newdata = .))
+
+players %>% ggplot(aes(R_hat)) + 
+    geom_histogram(binwidth = 0.5, color = "black")
+
+players <- Salaries %>% 
+    filter(yearID == 2002) %>%
+    select(playerID, salary) %>%
+    right_join(players, by="playerID")
+
+players <- Fielding %>% filter(yearID == 2002) %>%
+    filter(!POS %in% c("OF","P")) %>%
+    group_by(playerID) %>%
+    top_n(1, G) %>%
+    filter(row_number(G) == 1) %>%
+    ungroup() %>%
+    select(playerID, POS) %>%
+    right_join(players, by="playerID") %>%
+    filter(!is.na(POS)  & !is.na(salary))
+
+players <- Master %>%
+    select(playerID, nameFirst, nameLast, debut) %>%
+    right_join(players, by="playerID")
+
+players %>% select(nameFirst, nameLast, POS, salary, R_hat) %>% 
+    arrange(desc(R_hat)) %>% 
+    top_n(10) 
+
+players %>% ggplot(aes(salary, R_hat, color = POS)) + 
+    geom_point() +
+    scale_x_log10()
+
+players %>% filter(debut < 1998) %>%
+    ggplot(aes(salary, R_hat, color = POS)) + 
+    geom_point() +
+    scale_x_log10()
+
+
+### Regression Fallacy
+library(Lahman)
+playerInfo <- Fielding %>% 
+    group_by(playerID) %>% 
+    arrange(desc(G)) %>%
+    slice(1) %>%
+    ungroup %>% 
+    left_join(Master, by="playerID") %>% 
+    select(playerID, nameFirst, nameLast, POS)
+
+ROY <- AwardsPlayers %>%
+  filter(awardID == "Rookie of the Year") %>% 
+  left_join(playerInfo, by="playerID") %>%
+  rename(rookie_year = yearID) %>%
+  right_join(Batting, by="playerID") %>% 
+  mutate(AVG = H/AB) %>% 
+  filter(POS != "P")
+
+ROY <- ROY %>%  
+    filter(yearID == rookie_year | yearID == rookie_year+1) %>% 
+    group_by(playerID) %>% 
+    mutate(rookie = ifelse(yearID == min(yearID), "rookie", "sophomore")) %>%
+    filter(n() == 2) %>% 
+    ungroup %>%
+    select(playerID, rookie_year, rookie, nameFirst, nameLast, AVG) 
+
+ROY <- ROY %>% spread(rookie, AVG) %>% arrange(desc(rookie))
+
+two_years <- Batting %>% 
+    filter(yearID %in% 2013:2014) %>% 
+    group_by(playerID, yearID) %>%  
+    filter(sum(AB) >= 130) %>% 
+    summarize(AVG = sum(H)/sum(AB)) %>% 
+    ungroup %>% 
+    spread(yearID, AVG) %>% 
+    filter(!is.na(`2013`) & !is.na(`2014`)) %>%
+    left_join(playerInfo, by="playerID") %>% 
+    filter(POS!="P") %>% 
+    select(-POS) %>%
+    arrange(desc(`2013`)) %>% 
+    select(-playerID)
+
+two_years %>% 
+    ggplot(aes(`2013`, `2014`)) + 
+    geom_point()
+
+
+### Measurement Error Models
+library(dslabs)
+
+falling_object <- rfalling_object()
+falling_object %>% 
+    ggplot(aes(time, observed_distance)) + 
+    geom_point() +
+    ylab("Distance in meters") + 
+    xlab("Time in seconds")
+
+fit <- falling_object %>% 
+    mutate(time_sq = time^2) %>% 
+    lm(observed_distance~time+time_sq, data=.)
+tidy(fit)
+
+augment(fit) %>% 
+    ggplot() +
+    geom_point(aes(time, observed_distance)) + 
+    geom_line(aes(time, .fitted), col = "blue")
+
+
+
+### Confounding
+
